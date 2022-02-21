57d13c05b43146359925b6dc51bf87142d498ef7
Adam Campbell <adamrobcampbell@gmail.com>
2022-02-21 20:50:41 +1300

    Appears to work with multi-image

--- supy-res-imaging.py
+++ supy-res-imaging.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy.ndimage
 from scipy.signal import convolve2d
 from skimage.filters import gaussian
-from skimage import data, io
+from skimage import data, io, color
 
 import sys
 
@@ -11,23 +11,23 @@ import sys
 # Preliminary implementation of the Iterative Back Projection technique
 #======================================================================
 
+sample = color.rgb2gray(data.astronaut())
+
 # number of y low resolution images
-# (assumed linear in time, aka snapshots)
-num_low_res_images = 3
+num_low_res_images = 5
 # assumed to be square
-low_res_image_dim = 16
-high_res_image_dim = 32
+low_res_image_dim = 256
+high_res_image_dim = sample.shape[0]
 image_ratio = high_res_image_dim / low_res_image_dim
 
-error_threshold = 10.0
-max_iter = 10
+# error_threshold = 10.0
+max_iter = 20
 error = sys.float_info.max
+has_converged = False
 
-total_low_res_pixels = num_low_res_images * low_res_image_dim * low_res_image_dim
-# Initial estimate of low resolution images, assumed we get this provided to us
-low_res = np.arange(0, total_low_res_pixels, dtype=np.float32)
-# Rejig memory for fit ZYX axis layout
-low_res = np.reshape(low_res, (num_low_res_images, low_res_image_dim, low_res_image_dim))
+low_res = np.ndarray((num_low_res_images, low_res_image_dim, low_res_image_dim), np.float32)
+for k in range(num_low_res_images):
+    low_res[k] = scipy.ndimage.zoom(gaussian(sample), (1/image_ratio, 1/image_ratio), order=0)
 
 # 2D interpolation across YX axis, ignoring Z interpolation
 # note: order 0 = nearest, 1 = bilinear, 2 = cubic
@@ -40,7 +40,7 @@ high_res = np.average(low_res_upsampled, axis=0)
 # print(high_res)
 
 current_iter = 0
-while current_iter < max_iter:
+while current_iter < max_iter and not has_converged:
     current_iter += 1
     print("Processing iteration %d..." % current_iter)
 
@@ -54,7 +54,13 @@ while current_iter < max_iter:
     l2norm_sum = 0
     for k in range(num_low_res_images):
         l2norm_sum = l2norm_sum + np.linalg.norm(low_res[k] - low_res_predicted[k])
-    error = np.sqrt(1/num_low_res_images * l2norm_sum)
+    curr_err = np.sqrt(1/num_low_res_images * l2norm_sum)
+
+    if curr_err > error:
+        has_converged = True
+        break
+    
+    error = curr_err
     print("Error: %f..." % error)
 
     sharpen_kernel = np.array([[0, -1, 0],
@@ -75,10 +81,13 @@ while current_iter < max_iter:
     # Predict high resolution image
     high_res_predicted = np.zeros_like(high_res)
     sum_diff = np.sum(low_res_upsampled_diff, axis=0)
-    high_res_predicted = high_res + sum_diff * 1/num_low_res_images
+    weight = max(min(error, 1.0), 0.0) # clamp func for at most 1.0 multiplier
+    print("Iter %d weight: %f" % (current_iter, weight))
+    high_res_predicted = high_res + sum_diff * 1/num_low_res_images * weight
+    # io.imshow(np.absolute(high_res), plugin="matplotlib", **{"cmap": "Greys"})
+    # io.show()
     np.copyto(high_res, high_res_predicted)
+
     
-io.imshow(high_res)
-io.show()
 print("Finished processing...")
 sys.exit()
